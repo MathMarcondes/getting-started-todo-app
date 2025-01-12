93a289c53d64e4a62e4a4403886ec2a5caa3e06b
mathmarcondes <matheus.marcondes@outlook.com>
2025-01-12 20:42:48 -0300

    change the front

--- backend/src/routes/getGreeting.js
+++ backend/src/routes/getGreeting.js
@@ -2,6 +2,6 @@ const GREETING = 'Hello world!';
 
 module.exports = async (req, res) => {
     res.send({
-        greeting: GREETING,
+        greeting: GREETINGS[ Math.floor( Math.random() * GREETINGS.length )],
     });
 };
